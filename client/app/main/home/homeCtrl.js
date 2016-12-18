64ec09d8de9e69644301fff7df25ffe591c4126f
cozyazure <cozyazure@gmail.com>
2016-12-18 20:00:29 +0800

    successfully use html as markers, markers draggable

--- client/app/main/home/homeCtrl.js
+++ client/app/main/home/homeCtrl.js
@@ -2,6 +2,12 @@
     'use strict';
     angular.module('leaflet-overlay')
         .controller('homeCtrl', ["$scope", "$log", "leafletData", "leafletBoundsHelpers", function($scope, $log, leafletData, leafletBoundsHelpers) {
+            var divIcon = {
+                type: 'div',
+                iconSize: [500, 300],
+                popupAnchor: [0, 0],
+                html: '<img src="image/test.jpg" style="width:100%;"/>',
+            };
             angular.extend($scope, {
                 berlin: {
                     lat: 52.52,
@@ -11,7 +17,10 @@
                 markers: {
                     m1: {
                         lat: 52.52,
-                        lng: 13.40
+                        lng: 13.40,
+                        focus: false,
+                        icon: divIcon,
+                        draggable: true
                     }
                 },
                 layers: {
